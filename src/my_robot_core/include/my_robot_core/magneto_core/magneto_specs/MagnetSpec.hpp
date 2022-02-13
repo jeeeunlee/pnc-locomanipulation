a73dbf9bafd1463616048ff40e4c261f9f6e557e
jeeeunlee <jeeeun91.lee@gmail.com>
2022-02-13 13:51:09 -0600

    220213 update home

--- src/my_robot_core/include/my_robot_core/magneto_core/magneto_specs/MagnetSpec.hpp
+++ src/my_robot_core/include/my_robot_core/magneto_core/magneto_specs/MagnetSpec.hpp
@@ -4,23 +4,23 @@
 #include <Eigen/Dense>
 #include <iostream>
 
-#include <my_robot_system/RobotSystem.hpp>
+#include <my_wbc/Contact/ContactSpec.hpp>
+// #include <my_robot_system/RobotSystem.hpp>
 #include <my_utils/IO/IOUtilities.hpp>
 
 class MagnetSpec {
   public:
-    MagnetSpec(RobotSystem* _robot, int _link_idx, double _fm, double _res_ratio){
-        robot_ = _robot;        
-        link_idx_ = _link_idx;  
-        fm_=_fm;
-        residual_ratio_ = _res_ratio;   
+    MagnetSpec(ContactSpec* _contact, double _fm, double _res_ratio){
+      contact_ = _contact;
+      fm_=_fm;
+      residual_ratio_ = _res_ratio;   
 
-        contact_distance_ = 0.0; // assume in contact  
-        onoff_ = true;   
+      contact_distance_ = 0.0; // assume in contact  
+      onoff_ = true;   
     }
     ~MagnetSpec() {}
 
-    int getLinkIdx() {return link_idx_;}
+    int getLinkIdx() {return contact_->getLinkIdx();}
 
     void setContactDistance(double cd) { contact_distance_ = cd; }
     void setResidualRatio(double rr) { residual_ratio_ = rr; }
@@ -28,17 +28,20 @@ class MagnetSpec {
     void setMagnetOnoff(bool _onoff) { onoff_ = _onoff; }
 
     bool getOnOff() {return onoff_;}    
-    double getMagneticForce();    
+    Eigen::VectorXd getMagneticForce();    
     Eigen::MatrixXd getJacobian();
 
-  private:
-    double computeFm(double f0, double d=contact_distance_, double d0=0.02);
-
   protected:
-    RobotSystem* robot_;
-    int link_idx_;
+    ContactSpec* contact_;
     double fm_;
     double residual_ratio_;
     double contact_distance_;
     bool onoff_;
+
+  private:
+    double computeFm(double f0, double d=contact_distance_, double d0=0.02);
+    double computeFm(double f0);
+
+    Eigen::MatrixXd J_;
+    
 };
